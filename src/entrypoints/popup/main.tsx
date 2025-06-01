a64d9a5a1648ed9e012e06a8f9a8f6ae78aae03e
Husni Adil Makmur <husni.adil@gmail.com>
2025-06-01 11:32:05 +0700

    Integrate Tailwind CSS and shadcn/ui with dark mode support

--- src/entrypoints/popup/main.tsx
+++ src/entrypoints/popup/main.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import "@/styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
