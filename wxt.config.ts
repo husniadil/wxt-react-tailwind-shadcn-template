d6e840fed3e0112899a564789bd61b5020489dc7
Husni Adil Makmur <husni.adil@gmail.com>
2025-06-03 20:55:10 +0700

    Bump version to 0.1.0

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   srcDir: "./src",
   manifest: {
     name: APP_NAME,
-    version: "0.0.2",
+    version: "0.1.0",
     description: "manifest.json description",
     permissions: ["activeTab", "storage", "scripting"],
   },
